49ce02dd9bc0ddc9ad4be7cf8d41ce6dbc188b49
chansbae <csbae@ymail.com>
2014-06-10 05:43:19 +0900

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Ph.D. Changseok Bae on 2014. 6. 10..
+//  Copyright (c) 2014년 test. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
